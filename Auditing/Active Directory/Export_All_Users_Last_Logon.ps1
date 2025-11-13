ce6d7119df03d7f8c07554ddee46d61dc66cf510
capnhowyoudo <40323628+capnhowyoudo@users.noreply.github.com>
2025-11-13 16:38:57 -0600

    Update Export_All_Users_Last_Logon.ps1

--- Auditing/Active Directory/Export_All_Users_Last_Logon.ps1
+++ Auditing/Active Directory/Export_All_Users_Last_Logon.ps1
@@ -12,7 +12,7 @@ and you have sufficient permissions to query AD user accounts.
 Author      : capnhowyoudo
 Date        : 2025-11-12
 Requires    : ActiveDirectory module (RSAT) installed
-Output File : C:\alluser_reports.csv
+Output File : C:\Temp\alluser_reports.csv
 Usage       : Run the script in a session with AD privileges. Modify the output path as needed.
 #>
 
@@ -20,7 +20,7 @@ Usage       : Run the script in a session with AD privileges. Modify the output 
 Import-Module ActiveDirectory
 
 # Default output path
-$csvPath = "C:\alluser_reports.csv"
+$csvPath = "C:\Temp\alluser_reports.csv"
 
 # Retrieve enabled AD users and export LastLogonTimeStamp
 Get-ADUser -Filter {enabled -eq $true} -Properties LastLogonTimeStamp |
