3cea84c2ab5ad5d11dc4bf953d0ae8c138eab5c3
capnhowyoudo <40323628+capnhowyoudo@users.noreply.github.com>
2025-11-13 23:17:07 -0600

    Update Disable_Bulk_Users.ps1

--- Active-Directory/Disable_Bulk_Users.ps1
+++ Active-Directory/Disable_Bulk_Users.ps1
@@ -15,8 +15,8 @@ Author      : capnhowyoudo
 Date        : 2025-11-12
 Requires    : ActiveDirectory module (RSAT) installed
 CSV Format  : The CSV file should contain a column header named "samAccountName" Can aquire with 
-https://github.com/capnhowyoudo/Powershell-Scripts/blob/main/Active-Directory/Export_Enabled_Accounts.ps1
-https://github.com/capnhowyoudo/Powershell-Scripts/blob/main/Active-Directory/Export_Inactive_Users_Time_Span.ps1
+https://github.com/capnhowyoudo/Powershell-Scripts/blob/main/Active-Directory/Auditing/Get_Enabled_Accounts.ps1
+https://github.com/capnhowyoudo/Powershell-Scripts/blob/main/Active-Directory/Auditing/Get_Inactive_Users_N_Days.ps1
 Usage       : Modify the path to the CSV file and run the script in a session with AD privileges.
 
 .EXAMPLE
