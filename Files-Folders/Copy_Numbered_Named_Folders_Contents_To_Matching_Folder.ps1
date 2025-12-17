1c046a781d4fcc6d5b3700227d191f94b0a09a89
capnhowyoudo <40323628+capnhowyoudo@users.noreply.github.com>
2025-12-16 22:34:48 -0600

    Update Copy_Numbered_Named_Folders_Contents_To_Matching_Folder.ps1

--- Files-Folders/Copy_Numbered_Named_Folders_Contents_To_Matching_Folder.ps1
+++ Files-Folders/Copy_Numbered_Named_Folders_Contents_To_Matching_Folder.ps1
@@ -14,16 +14,16 @@
     Log file: C:\Temp\CopyLog.csv
     The script overwrites existing files in the destination folder.
 
-.EXAMPLES
-    # Example of folder matching:
-    # Source folder:
-    #   C:\Temp\Source\documents\12345678
-    #
-    # Matching destination folder:
-    #   C:\Temp\Destination\documents\Backup\ProjectXYZ\DOC-9876543-12345678_config.conf
-    #
-    # The script will copy all files from the source folder "12345678" 
-    # into the matching destination folder that contains the same number "12345678".    
+.EXAMPLE
+    Example of folder matching:
+    Source folder:
+    C:\Temp\Source\documents\12345678
+    
+    Matching destination folder:
+    C:\Temp\Destination\documents\Backup\ProjectXYZ\DOC-9876543-12345678_config.conf
+    
+    The script will copy all files from the source folder "12345678" 
+    into the matching destination folder that contains the same number "12345678".    
 #>
 
 # Set source and destination root paths
